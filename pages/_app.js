4d92b3861355507b718cfc8b0dc555b046c77267
Shaazgai <bzao.hover@gmail.com>
2022-06-30 23:37:18 +0800

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/Main.css'
+import '../styles/Navbar.css'
+import '../styles/Title.css'
+import '../styles/Action.css'
+import '../styles/Splide.css'
+import '../styles/Layout.css'
+import Layout from './Components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+    <Layout>
+   
+  <Component {...pageProps} />
+  </Layout> 
+  )
+
+
 }
 
 export default MyApp
